9456131acba87a6735e91219dcb55e4c08b808b8
omegatech-andre <omega.ti_ind@outlook.com>
2024-01-09 16:14:35 -0300

    fix:{alteração do qrcode}

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import '../styles/globals.scss'
 import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
-  title: 'Ômega Screen | Tintas Serigráficas',
+  title: 'Ômega Screen | Comercial',
   description: 'Indústria especializada em tintas serigráficas, produzindo um padrão de qualidade desde 2021 é referência em Pernambuco.'
 }
 
